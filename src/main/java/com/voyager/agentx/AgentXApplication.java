d247eb8f29a76e2aca89aebeebef58201c68a801
LiuKai <3513864951@qq.com>
2025-04-25 10:36:00 +0800

    :tada: Initial commit

---
+++ src/main/java/com/voyager/agentx/AgentXApplication.java
@@ -0,0 +1,63 @@
+package com.voyager.agentx;
+
+import io.micrometer.common.util.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import java.net.UnknownHostException;
+import java.net.InetAddress;
+
+@SpringBootApplication
+@Slf4j
+public class AgentXApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(AgentXApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        app.setBannerMode(Banner.Mode.CONSOLE);
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/doc.html";
+        } else {
+            contextPath = contextPath + "/doc.html";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("""
+                        ----------------------------------------------------------
+                        \t应用程序“{}”正在运行中......
+                        \t接口文档访问 URL:
+                        \t本地: \t\t{}://localhost:{}{}
+                        \t外部: \t{}://{}:{}{}
+                        \t配置文件: \t{}
+                        ----------------------------------------------------------""",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles());
+    }
+
+}
---
+++ src/test/java/com/voyager/agentx/ThinkForgeAiAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voyager.agentx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkForgeAiAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
